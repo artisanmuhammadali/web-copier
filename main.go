fdb3bb76bf83c002d48caeffcfbb25e31124ce7b
artisanmuhammadali <artisanmuhammadali@gmail.com>
2024-07-10 01:25:07 +0500

    extract a tags from the html body then send request on them and make files for them with sub directories if needed

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -19,16 +22,10 @@ func main() {
 
 	url := inputUrl()
 
-	res, err := http.Get(url)
-	handleErr(err)
-	defer res.Body.Close()
+	body, _ := getPageBody(url)
 
-	body, err := io.ReadAll(res.Body)
-	handleErr(err)
+	parseResBodyAsHtml(string(body), url, dir)
 
-	path := dir + "/index.html"
-	file := os.WriteFile(path, body, 0644)
-	handleErr(file)
 	fmt.Println("Website Copy successfully...")
 }
 
@@ -55,8 +52,64 @@ func inputUrl() string {
 	url = strings.TrimSpace(url)
 	return url
 }
+
+func parseResBodyAsHtml(body string, url string, dir string) {
+	doc, err := html.Parse(strings.NewReader(body))
+	handleErr(err)
+	extractAnchorTags(doc, url, dir)
+}
+func extractAnchorTags(doc *html.Node, url string, dir string) {
+	if doc.Type == html.ElementNode && doc.Data == "a" {
+		for _, attr := range doc.Attr {
+			if attr.Key == "href" {
+				result := strings.Contains(attr.Val, url)
+				if result {
+					body, _ := getPageBody(attr.Val)
+					createFile(attr.Val, url, dir, body)
+				}
+			}
+		}
+	}
+	for i := doc.FirstChild; i != nil; i = i.NextSibling {
+		extractAnchorTags(i, url, dir)
+	}
+}
 func handleErr(err error) {
 	if err != nil {
 		fmt.Print(err)
 	}
+}
+func createFile(url string, baseUrl string, dir string, body []byte) {
+	fileName := strings.Replace(url, baseUrl, "", -1)
+	if fileName == "" {
+		fileName = "index"
+	}
+	path := dir + "/" + fileName + ".html"
+
+	file := os.WriteFile(path, body, 0644)
+	if file != nil {
+		parts := strings.Split(path, "/")
+
+		lastPart := parts[len(parts)-1]
+		if filepath.Ext(lastPart) != "" {
+			parts = parts[:len(parts)-1]
+		}
+		checkdir := filepath.Join(parts...)
+		if _, err := os.Stat(checkdir); os.IsNotExist(err) {
+			err := os.MkdirAll(checkdir, 0755)
+			handleErr(err)
+			createFile(url, baseUrl, dir, body)
+		} else {
+			fmt.Println("The provided directory named", dir, "exists")
+		}
+	}
+}
+func getPageBody(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	handleErr(err)
+
+	body, err := io.ReadAll(res.Body)
+	handleErr(err)
+	res.Body.Close()
+	return body, nil
 }
